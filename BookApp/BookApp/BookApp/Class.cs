fb5c9f74fb9b2a778c921e309e2b6abe16a276cc
Jiivatti <Jiivatti>
2019-01-09 14:31:15 +0200

    Created the book app.

---
+++ BookApp/BookApp/BookApp/Class.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookApp
+{
+    class Book
+    {
+        public string name;
+        public string author;
+        public int ID;
+        public double price;
+
+        public Book(string name, string author, int ID, double price)
+        {
+            this.name = name;
+            this.author = author;
+            this.ID = ID;
+            this.price = price;
+        }
+
+        public void printInfo()
+        {       Console.WriteLine($"Book name: {this.name} \n" +
+                $"Author: {this.author} \n" +
+                $"ID number: {this.ID} \n" +
+                $"Price: {this.price} euro \n" +
+                $"---------------------------------------- \n");
+
+        }
+
+        public string compareInfo(Book book)
+        {
+            if (this.price > book.price)
+                return $"\n{this.name} ({this.price}e) is more expensive than {book.name} ({book.price}e). ";
+            else if(this.price == book.price)
+                return $"\nNo difference in price. ";
+            else
+                return $"\n{book.name} ({book.price}e) is more expensive than {this.name} ({this.price}e).";
+        }
+    }
+}
---
+++ BookApp/BookApp/BookApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BookApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = null;
+            Book Book1 = new Book("Bad Book", "Terribad Author", 15582, 72.50);
+            Book Book2 = new Book("Kickass Book", "Me :----D", 99998, 89.90);
+            Book Book3 = new Book("Beep Boop", "Mr.Roboto", 87602, 2.50);
+
+            do
+            {
+                choice = userInterface();
+
+                switch (choice.ToUpper())
+                {
+                    case "Q":
+                        Book1.printInfo(); Book2.printInfo(); Book3.printInfo();
+                        break;
+
+                    case "W":
+                        Console.WriteLine(Book1.compareInfo(Book2));
+                        Console.WriteLine(Book1.compareInfo(Book3));
+                        Console.WriteLine(Book2.compareInfo(Book1));
+                        Console.WriteLine(Book2.compareInfo(Book3));
+                        break;
+                }
+            } while (choice.ToUpper() != "F");
+
+            string userInterface()
+            {
+                Console.WriteLine("\nPress [Q] for a list of all available books. ");
+                Console.WriteLine("Press [W] to compare book prices. ");
+                Console.WriteLine("Press [F] to quit the program. ");
+                return Console.ReadLine();
+            }
+        }
+    }
+}
