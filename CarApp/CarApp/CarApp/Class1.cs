52e1c5c57843d7f26721d99c0848f05b713d7fcd
Jiivatti <Jiivatti>
2019-01-09 14:26:12 +0200

    Car app created

---
+++ CarApp/CarApp/CarApp/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarApp
+{
+    class Cars
+    {
+        public string brand;
+        public double speed;
+
+
+        public Cars()
+        {
+            this.brand = null;
+            this.speed = 0;
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"---------------------" +
+                $"\nCar brand: {this.brand}\nTop speed: {this.speed}kmph\n");
+        }
+
+        public string AskData()
+        {
+            Console.WriteLine("\nInsert car brand: ");
+            this.brand = Console.ReadLine();
+            Console.WriteLine("\nInsert car speed: ");
+            this.speed = int.Parse(Console.ReadLine());
+            return $"\nYour car brand is now {this.brand}, and it's top speed is {this.speed}kmph\n---------------------";
+        }
+
+        public string Accelerate()
+        {
+            this.speed = this.speed * 1.1;
+            this.speed = Math.Round(this.speed, 1);
+            return $"\nTurbo boosted {this.brand}'s speed to {this.speed}kmph\n";
+        }
+
+        public string Decelerate()
+        {
+            this.speed = this.speed * 0.90;
+            this.speed = Math.Round(this.speed, 1);
+            return $"\nDecelerated {this.brand}'s speed to {this.speed}kmph\n";
+        }
+    }
+}
---
+++ CarApp/CarApp/CarApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars car1 = new Cars();
+            Cars car2 = new Cars();
+            Console.WriteLine(car1.AskData());
+            string choice = null;
+
+
+            do
+            {
+                choice = userInterface();
+
+                switch (choice.ToUpper())
+                {
+                    case "Q":
+                        Console.WriteLine(car2.AskData());
+                        break;
+
+                    case "W":
+                        car1.ShowCarInfo();
+                        if (car2.brand != null)
+                        {
+                            car2.ShowCarInfo();
+                        } else break;
+                        break;
+
+                    case "R":
+                        Console.WriteLine(car1.Accelerate());
+                        if (car2.speed != 0)
+                        {
+                            Console.WriteLine(car2.Accelerate());
+                        } else break;
+                        break;
+
+                    case "E":
+                        Console.WriteLine(car1.Decelerate());
+                        if (car2.speed != 0)
+                        {
+                            Console.WriteLine(car2.Decelerate());
+                        } else break;
+                        break;
+                }
+            } while (choice.ToUpper() != "F");
+
+
+
+            string userInterface()
+            {
+                Console.WriteLine("[Q] Add another car");
+                Console.WriteLine("[W] Show registered cars");
+                Console.WriteLine("[E] Decelerate car(s)");
+                Console.WriteLine("[R] Turbo boost car(s)");
+                Console.WriteLine("[F] End program");
+                return Console.ReadLine();
+            }
+        }
+    }
+}
